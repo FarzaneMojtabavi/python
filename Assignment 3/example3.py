86c227542b83045441c8eaa4694919a88a075dd5
Farzane Mojtabavi <96921828+FarzaneMojtabavi@users.noreply.github.com>
2023-01-06 23:08:30 +0330

    Add files via upload

---
+++ Assignment 3/example3.py
@@ -0,0 +1,24 @@
+# *******************************************************************
+# yek araye az karbar migire v moshakhas mikone sort has ya na.az kochik b bozorg
+# listnumber=[]
+# while True:
+#     n=int(input('n: '))
+#     listnumber.append(n)
+#     if n==100:
+#         break   
+# for i in range(len(listnumber)-1):
+#     if listnumber[i]<listnumber[i+1]:
+#         s='yes'
+#     else:
+#         s='no'      
+#         break     
+# print(listnumber)
+# print(s)
+# *******************************************************************
+# snake
+# n=int(input())
+# for i in range(n):
+#     if i%2==0:
+#         print('*',end='')
+#     else:
+#         print('#',end='')    
