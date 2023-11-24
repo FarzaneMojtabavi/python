4795257527e8f9eaed0965d200aa686246b5f29c
Farzane Mojtabavi <96921828+FarzaneMojtabavi@users.noreply.github.com>
2023-11-24 19:55:08 +1030

    Update a4.py

--- Assignment 4/Assignment 4 new/a4.py
+++ Assignment 4/Assignment 4 new/a4.py
@@ -5,3 +5,17 @@ def r(t):
             count+=1
     print(count)
 r('aaa bbb ccc')
+
+or:
+def w(ww):
+    word=1
+    lenword=0
+    space=0
+    for i in ww:
+        if i==' ':
+            word+=1
+            space+=1
+        else:
+            lenword+=1
+    print('word:',word,'lenword:',lenword,'space:',space)
+w(input())
